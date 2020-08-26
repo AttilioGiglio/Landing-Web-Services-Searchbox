19c12fd9e6be52a2265d9dc5f7bc6c380c0a2e1a
attiliogiglio <attilio.g93@gmail.com>
2020-08-26 09:20:44 -0400

    debugging

--- src/Actions.js
+++ src/Actions.js
@@ -13,7 +13,10 @@ export const setSearchField = (text) => ({
 
 export const requestRobots = () => (dispatch) => {
     dispatch({ type: REQUEST_ROBOTS_PENDING })
-    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(data => ({ type: REQUEST_ROBOTS_SUCCESS, payload: data })).catch(error => dispatch({type: REQUEST_ROBOTS_FAILED, payload:error}))
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
+    .then(data => {
+        return dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data })
+    }).catch(error => dispatch({type: REQUEST_ROBOTS_FAILED, payload:error}))
 };
 
 // export const setSearchField = (text) => ({
